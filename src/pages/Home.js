218fc8eef8d17670b71bc0f1eac6fe12836f40dc
Sanespim <sanespim99@gmail.com>
2023-07-18 15:54:30 -0500

    Api Conexión

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import Axios  from 'axios';
+import { useEffect, useState } from 'react';
 
 
 const Home = ()=> {
   const {user, isAuthenticated,isLoanding} = useAuth0();
-
+const key = 'GmyLPHafWz8zF/AY9l+OdQ==bOENjcyCrvXEqPiU';
+const headers= {
+  'X-Api-Key': key
+}
+const url = 'https://api.api-ninjas.com/v1/celebrity?name='+'Michael Jordan';
+useEffect (()=>{
+  Axios.get (url)
+  .then (rest=>{console.log(rest.data)}
+  )
+  .catch(error=>{console.log(error)})
+},[]
+)
   if (isLoanding) {
     return <div>Cargando...</div>;
   }
