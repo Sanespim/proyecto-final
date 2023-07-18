4c0379254381b1a3ebbd163a6bfa9f1ed4d8e6bb
Sanespim <sanespim99@gmail.com>
2023-07-18 17:34:55 -0500

    Home   funcional

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Axios  from 'axios';
 import { useEffect, useState } from 'react';
 
 
 const Home = ()=> {
-  const {user, isAuthenticated,isLoanding} = useAuth0();
+  const {user} = useAuth0();
+  const [celebrity, setCelebrity] = useState([]);
 const key = process.env.REACT_APP_KEY;
 const headers= {
   'X-Api-Key': key
@@ -14,21 +15,38 @@ const nombre = 'Michael Jordan';
 const url = process.env.REACT_APP_URL_APP+nombre;
 useEffect (()=>{
   Axios.get (url, {headers})
-  .then (rest=>{console.log(rest.data)}
-  )
+  .then (rest=>{console.log(rest.data)
+  setCelebrity(rest.data)
+  console.log(celebrity)
+})
   .catch(error=>{console.log(error)})
 },[]
 )
-  if (isLoanding) {
-    return <div>Cargando...</div>;
-  }
+  
   return(
-    isAuthenticated && (
-      <div>
-        <h3>{user.name}</h3>
-      </div>
+     <div><h3>{user.name}</h3>
+     <div>
+      {celebrity.map(cel=>{return(
+        <Fragment>
+        <h4>{cel.name}</h4>
+        <h2>{cel.age}</h2>
+        {cel.occupation.map(prof=>{
+          return (
+            <p>{prof}</p>
+          )
+        }
+
+        )}
+        </Fragment>
+
+      )})}
+     </div>
+     
+     </div>
+
+
+      
     )
-  );
 }
 
 export default Home;
